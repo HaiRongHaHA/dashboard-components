9c01fa0cac82d8c2688bcc54391e89f0010d66d9
HaiRongHaHA <whr9743@163.com>
2023-11-30 17:51:02 +0800

    feat: 分包

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,11 +1,11 @@
 import * as path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import { dcRoot, pkgRoot } from '../build/paths'
+import { dcRoot, pkgRoot } from '../build/paths'
 
-export const projRoot = path.resolve(__dirname, '..')
-export const pkgRoot = path.resolve(projRoot, 'packages')
-export const dcRoot = path.resolve(pkgRoot, 'dc-components')
+// export const projRoot = path.resolve(__dirname, '..')
+// export const pkgRoot = path.resolve(projRoot, 'packages')
+// export const dcRoot = path.resolve(pkgRoot, 'dc-components')
 
 // https://vitejs.dev/config/
 export default defineConfig({
